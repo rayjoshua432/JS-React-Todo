b1fac959487dcfd08d21a57c6ec9eedec2fb5c10
Ray Joshua Balingkit <rayjoshua432@gmail.com>
2022-06-26 20:36:12 +0800

    Todo List

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const Landing = () => {
+   // Hook
+   const navigation = useNavigate()
+   const clickedButton = () => {
+      navigation('/todo')
+   }
+
    return (
       <div className='landing-container'>
-         <div className='landing-desc'>
-            <h2>Hello there!</h2>
-            <p>Where do you want to go?</p>  
-            <button><a href='/'>Home</a></button>
-            <button><a href='/todo'>Todo</a></button>
+         <div className='landing-main'>
+            <h1>My Todo List</h1>
+            <p>Create your todo list now.</p>
+            <button onClick={clickedButton}>Start Now</button>
          </div>
       </div>
    )
--- src/components/todo.js
+++ src/components/todo.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Todo = () => {
    return (
-      <div> Welcome to the Todo Page!</div>
+      <div className='todo-container'>
+         <div className='todo-desc'>
+            <h2>Todo List</h2>
+            <p>Do you have something to do?</p>
+            <input type='text' placeholder='Make a coffee...'></input>
+            <button>Add</button>
+         </div>
+         <table className='todo-table'>
+            <thead>
+               <tr>
+                  <th>Task</th>
+                  <th>Option</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>Do laundry</td>
+                  <td>
+                     <button>Edit</button>
+                     <button>Delete</button>
+                  </td>
+               </tr>
+            </tbody>         
+         </table>
+      </div>
    )
 }
 
