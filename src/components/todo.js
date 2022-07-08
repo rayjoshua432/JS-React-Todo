768b75b7a561a28681c26669464e18696116fb78
Ray Joshua Balingkit <rayjoshua432@gmail.com>
2022-07-09 06:22:43 +0800

    Updated React App

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,27 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todo = () => {
+   // State functions
+   const [state, setState] = useState({
+      todo: '',
+      todolist: []
+   })
+   const { todo, todolist } = state
+
+   // Onchange handler
+   const changeHandler = (e) => {
+      const { name, value } = e.target
+
+      setState({...state, [name]: value})
+   }
+   //onClick handler
+   const addTodo = () => {
+      const list = todolist
+      list.push(todo)
+      
+      setState({...state, todolist: list})
+   }
+   const deleteTodo = () => {
+      const list = todolist
+  
+   }
+
    return (
       <div className='todo-container'>
          <div className='todo-desc'>
             <h2>Todo List</h2>
             <p>Do you have something to do?</p>
-            <input type='text' placeholder='Make a coffee...'></input>
-            <button>Add</button>
+            <input type="text" placeholder='Make a coffee...' defaultValue={todo} onChange={changeHandler}></input>
+            <button onClick={addTodo}>Add</button>
          </div>
          <div className='todo-list'>
             <div className='todo-header'>
                <span>Task</span>
                <span>Options</span>
             </div>
-            <div className='todos'>
-               <span>Do Laundry</span>
-               <button>Edit</button>
-               <button>Delete</button>
-               <span>Exercise - Walking</span>
-               <button>Edit</button>
-               <button>Delete</button>
-            </div>
+            {
+               todolist.length ?
+                  todolist.map((value, index) => {
+                     return (
+                        <div key={index} className='todos'>
+                           <span>{value}</span>
+                           <button>Edit</button>
+                        <button onClick={deleteTodo}>Delete</button>
+                        </div>
+                     )
+                  }) : <span className='err'>Nothing to show here.</span>
+            }
          </div>
       </div>
    )
